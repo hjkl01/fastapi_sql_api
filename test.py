d2ecb722f235147c49bc53f9742e220209406fc7
The handsome boy <762293490@qq.com>
2021-11-23 10:29:16 +0800

    update client

--- test.py
+++ test.py
@@ -9,20 +9,21 @@ from requests.auth import HTTPBasicAuth
 load_dotenv()
 
 
-host = "127.0.0.1"
+host = "127.0.0.1:8000"
 
 urls = [
-    f"http://{host}:8000/lpush/",
-    f"http://{host}:8000/rpop/",
+    f"http://{host}/lpush/",
+    f"http://{host}/rpop/",
 ]
 for i in range(3):
     for url in urls:
         print(url)
         value = {f"dslakjdlksa{i}": f"dsljakdjsal{i}"}
+        value = {f"广州{i}": f"dsljakdjsal{i}"}
         data = {"db": 3, "key": "test", "value": value}
         resp = requests.post(
             url,
-            data=json.dumps(data, ensure_ascii=False),
+            data=json.dumps(data, ensure_ascii=False).encode("utf-8"),
             auth=HTTPBasicAuth(os.getenv("user"), os.getenv("password")),
         )
         logger.info(f"{resp.status_code}, {i}, {resp.json()}")
