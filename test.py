0e510467224acaec4288a21ba67588c2e0d01729
The handsome boy <ycm76229@gmail.com>
2022-01-25 14:47:24 +0800

    format python file

--- test.py
+++ test.py
@@ -8,12 +8,15 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 
-Config = Dynaconf(settings_files=['.secrets.toml'])
+Config = Dynaconf(settings_files=[".secrets.toml"])
 
 
 def send_request(url, data):
-    resp = requests.post(url, data=json.dumps(data, ensure_ascii=False).encode(
-        "utf-8"), auth=HTTPBasicAuth(Config.user, Config.password))
+    resp = requests.post(
+        url,
+        data=json.dumps(data, ensure_ascii=False).encode("utf-8"),
+        auth=HTTPBasicAuth(Config.user, Config.password),
+    )
     logger.info(f"{resp.status_code}, {resp.json()}")
     return resp
 
@@ -29,19 +32,32 @@ def redis_test():
             value = {f"dslakjdlksa{i}": f"dsljakdjsal{i}"}
             value = {f"广州{i}": f"dsljakdjsal{i}"}
             data = {"db": 3, "key": "test", "value": value}
-            t = Thread(target=send_request, args=(url, data,))
+            t = Thread(
+                target=send_request,
+                args=(
+                    url,
+                    data,
+                ),
+            )
             t.start()
             time.sleep(0.1)
 
 
 def mongo_test():
-    url = f'http://{Config.host}/mongo/insert/'
-    for i in range(100):
-        db = 'test'
-        tablename = 'test'
-        values = {'_id': i, 'values': f'value_{i}'}
-        data = {'db': db, 'tablename': tablename, 'values': values}
-        t = Thread(target=send_request, args=(url, data,))
+    url = f"http://{Config.host}/mongo/insert/"
+    for i in range(10):
+        print(url)
+        db = "test"
+        tablename = "test"
+        values = {"_id": i, "values": f"value_{i}"}
+        data = {"db": db, "tablename": tablename, "values": values}
+        t = Thread(
+            target=send_request,
+            args=(
+                url,
+                data,
+            ),
+        )
         t.start()
         time.sleep(0.1)
 
